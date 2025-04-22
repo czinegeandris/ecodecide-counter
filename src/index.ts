022d4a58957e40324d3bb417d1690f7c8c886d55
Czinege András <80851806+czinegeandris@users.noreply.github.com>
2025-04-23 00:57:17 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -39,10 +39,10 @@ export default {
         count = await stub.getCounterValue();
         break;
       default:
-        return new Response("Not found", { status: 404 });
+        return new Response(JSON.stringify({ error: "Not found" }), {status: 404, headers: { 'Content-Type': 'application/json' }});
     }
 
-    return new Response(`Durable Object '${name}' count: ${count}`);
+    return new Response(JSON.stringify({ name, count }), {headers: { 'Content-Type': 'application/json' }});
   },
 } satisfies ExportedHandler<Env>;
 
